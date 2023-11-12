8168309bd3511e59af0386447fe5a62b859746d6
ViniciosLugli <vinicioslugli@gmail.com>
2023-11-12 20:20:27 -0300

    feat(node): :sparkles: Created code of node following point and added map yaml

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 2023-2A-T2-M8-P1
-2023 - Módulo 8 - Ponderada 1
+
+## Demo:
--- project_ws/controller_package/controller_package/node.py
+++ project_ws/controller_package/controller_package/node.py
@@ -5,33 +5,42 @@ from tf_transformations import quaternion_from_euler
 from math import pi
 
 
-def create_pose_stamped(navigator, pos_x, pos_y, rot_z):
-    q_x, q_y, q_z, q_w = tf_transformations.quaternion_from_euler(0.0, 0.0, rot_z)
-    pose = PoseStamped()
-    pose.header.frame_id = 'map'
-    pose.header.stamp = nav.get_clock().now().to_msg()
-    pose.pose.position.x = pos_x
-    pose.pose.position.y = pos_y
-    pose.pose.position.z = pos_x
-    pose.pose.orientation.x = q_x
-    pose.pose.orientation.y = q_y
-    pose.pose.orientation.z = q_z
-    pose.pose.orientation.w = q_w
-    return pose
+class Navigator:
+    def __init__(self):
+        self.navigator = BasicNavigator()
+
+    def create_pose(self, pos_x, pos_y, rot_z):
+        q_x, q_y, q_z, q_w = quaternion_from_euler(0.0, 0.0, rot_z)
+        pose = PoseStamped()
+        pose.header.frame_id = 'map'
+        pose.header.stamp = self.navigator.get_clock().now().to_msg()
+        pose.pose.position.x = pos_x
+        pose.pose.position.y = pos_y
+        pose.pose.position.z = pos_x
+        pose.pose.orientation.x = q_x
+        pose.pose.orientation.y = q_y
+        pose.pose.orientation.z = q_z
+        pose.pose.orientation.w = q_w
+        return pose
+
+    def follow_waypoints(self, waypoints):
+        self.navigator.followWaypoints(waypoints)
+        while not self.navigator.isTaskComplete():
+            print(self.navigator.getFeedback())
 
 
 def main():
     rclpy.init()
-    nav = BasicNavigator()
-    goal_pose1 = create_pose_stamped(nav, 2.5, 1.0, 1.57)
-    goal_pose2 = create_pose_stamped(nav, 0.0, 1.0, 1.57)
-    goal_pose3 = create_pose_stamped(nav, 0.0, 0.0, 0.00)
 
-    waypoints = [goal_pose1, goal_pose2, goal_pose3]
+    navigator = Navigator()
 
-    nav.followWaypoints(waypoints)
-    while not nav.isTaskComplete():
-        print(nav.getFeedback())
+    waypoints = [
+        navigator.create_pose(3.5, 2.0, 3.5),
+        navigator.create_pose(0.0, 1.5, 1.5),
+        navigator.create_pose(0.0, 0.0, 0.0),
+    ]
+
+    navigator.follow_waypoints(waypoints)
 
     rclpy.shutdown()
 
